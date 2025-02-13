80fa21230d1bd4f26c1e3d2c9004fa790f64070a
MochaMousse <mocha@mousse.cc>
2025-02-13 16:38:22 +0800

    添加AI调用

--- src/main/java/cc/mousse/steward/StewardExchangeVelocity.java
+++ src/main/java/cc/mousse/steward/StewardExchangeVelocity.java
@@ -3,19 +3,37 @@ package cc.mousse.steward;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.player.PlayerChatEvent;
+import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
 
 /**
  * @author MochaMousse
  */
+@Slf4j
 @Plugin(id = "steward-exchange-velocity", name = "steward-exchange-velocity", version = "2025.1.20")
 public class StewardExchangeVelocity {
+  private static final String AI_URL = "http://127.0.0.1:1237/mc";
   private static final MinecraftChannelIdentifier IDENTIFIER =
       MinecraftChannelIdentifier.from("steward:exchange");
+  private static final OkHttpClient CLIENT =
+      new OkHttpClient()
+          .newBuilder()
+          .connectTimeout(10, TimeUnit.MINUTES)
+          .readTimeout(10, TimeUnit.MINUTES)
+          .writeTimeout(10, TimeUnit.MINUTES)
+          .callTimeout(10, TimeUnit.MINUTES)
+          .build();
+  private static final Gson GSON = new Gson();
   private final ProxyServer proxyServer;
 
   @Inject
@@ -25,24 +43,64 @@ public class StewardExchangeVelocity {
   }
 
   @Subscribe
-  public void onPlayChat(PlayerChatEvent event) {
-    event
-        .getPlayer()
-        .getCurrentServer()
-        .ifPresent(
-            serverConnection -> {
-              String server = serverConnection.getServerInfo().getName();
-              String player = event.getPlayer().getUsername();
-              String content = event.getMessage();
-              proxyServer
-                  .getAllServers()
-                  .forEach(
-                      registeredServer ->
-                          registeredServer.sendPluginMessage(
-                              IDENTIFIER,
-                              new Gson()
-                                  .toJson(new Letter(server, player, content))
-                                  .getBytes(StandardCharsets.UTF_8)));
-            });
+  public void onPluginMessage(PluginMessageEvent event) {
+    proxyServer
+        .getScheduler()
+        .buildTask(
+            this,
+            () -> {
+              if (Objects.equals(event.getIdentifier(), IDENTIFIER)) {
+                broadcast(event.getData());
+                String data = new String(event.getData(), StandardCharsets.UTF_8);
+                Letter letter = GSON.fromJson(data, Letter.class);
+                if (letter.ai == 1) {
+                  Request request =
+                      new Request.Builder()
+                          .url(AI_URL)
+                          .post(
+                              RequestBody.create(
+                                  data, MediaType.get("application/json; charset=utf-8")))
+                          .build();
+                  try (Response response = CLIENT.newCall(request).execute()) {
+                    String message;
+                    if (response.isSuccessful()) {
+                      message =
+                          GSON.fromJson(
+                                  Objects.requireNonNull(response.body()).string(),
+                                  ResponseData.class)
+                              .data;
+                    } else {
+                      message = "服务暂时不可用";
+                    }
+                    broadcast(new Letter(-1, letter.server, letter.player, message));
+                  } catch (IOException e) {
+                    log.warn("无法调用AI", e);
+                    broadcast(new Letter(-1, letter.server, letter.player, "服务暂时不可用"));
+                  }
+                }
+              }
+            })
+        .schedule();
   }
+
+  private void broadcast(Letter letter) {
+    broadcast(GSON.toJson(letter).getBytes(StandardCharsets.UTF_8));
+  }
+
+  private void broadcast(byte[] letter) {
+    proxyServer
+        .getAllServers()
+        .forEach(registeredServer -> registeredServer.sendPluginMessage(IDENTIFIER, letter));
+  }
+
+  @Data
+  @AllArgsConstructor
+  public static class Letter {
+    private Integer ai;
+    private String server;
+    private String player;
+    private String content;
+  }
+
+  public record ResponseData(String data) {}
 }
